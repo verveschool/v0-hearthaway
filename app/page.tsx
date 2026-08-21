81f4805b6bdc0d8826e167b8d22f944c5c8f5f4a
A. Duggal <84291846+verveschool@users.noreply.github.com>
2026-08-21 15:07:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,21 @@ import HeroSection from '@/components/home/hero-section'
 import AccommodationPartnersSection from '@/components/home/accommodation-partners-section'
 import TrustStatsSection from '@/components/home/trust-stats-section'
 import HowItWorks from '@/components/home/how-it-works'
-import GuidanceSection from '@/components/home/guidance-section'
-import AccommodationTypes from '@/components/home/accommodation-types'
-import DestinationsSection from '@/components/home/destinations-section'
-import TrustSection from '@/components/home/trust-section'
-import MovingAbroadPreview from '@/components/home/moving-abroad-preview'
 import CtaSection from '@/components/home/cta-section'
 
 export default function HomePage() {
   return (
     <>
       <Navigation />
+
       <main>
         <HeroSection />
         <AccommodationPartnersSection />
         <TrustStatsSection />
         <HowItWorks />
-        <GuidanceSection />
-        <AccommodationTypes />
-        <DestinationsSection />
-        <TrustSection />
-        <MovingAbroadPreview />
         <CtaSection />
       </main>
+
       <Footer />
     </>
   )
