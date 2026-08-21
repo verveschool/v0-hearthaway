106c86a8d811fea459d0eab16d8fe6bb4f3a406c
A. Duggal <84291846+verveschool@users.noreply.github.com>
2026-08-21 17:07:04 +0530

    Update sitemap to use hearthaway.com domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://v0-hearthaway.vercel.app'
+  const baseUrl = 'https://hearthaway.com'
 
   // Define static routes
   const staticRoutes = [
